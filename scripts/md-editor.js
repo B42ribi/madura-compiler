33a6a86df3631c644f573ad8f16af09f622b0e7d
Feilen <Manuel.Feilen@adesso.de>
2020-06-12 23:53:03 +0200

    introduced linenumbering

--- scripts/md-editor.js
+++ scripts/md-editor.js
@@ -1,13 +1,16 @@
 (function () {
 
-	const TAB_KEY = 9;
-	const BACKSPACE = 8;
+	const ENTER = 'Enter';
+	const TAB_KEY = 'Tab';
+	const BACKSPACE = 'Backspace';
 
 	class MdEditor extends HTMLElement {
 
 		constructor() {
 			super();
+		}
 
+		connectedCallback() {
 			let panel = document.createElement('div');
 			panel.classList.add('md-editor');
 			panel.contentEditable = true;
@@ -15,13 +18,18 @@
 			let button = document.createElement('button');
 			button.innerHTML = "Parse";
 
-			panel.addEventListener('keydown', (event) => {
-				switch (event.which) {
-					case BACKSPACE: break;
+			this.addEventListener('keydown', (event) => {
+				switch (event.key) {
+					case ENTER:
+						event.preventDefault();
+						console.log(window.getSelection());
+						createLine(panel, '\xA0');
+						break;
 					case TAB_KEY:
 						event.preventDefault();
 						document.execCommand('insertText', false, '    ');
 						break;
+					case BACKSPACE: break;
 				}
 			});
 
@@ -39,14 +47,37 @@
 			style.rel = 'stylesheet';
 			style.href = "styles/md-editor.css";
 
-			let shadowRoot = this.attachShadow({ mode: 'closed' });
-			shadowRoot.appendChild(style);
-			shadowRoot.appendChild(panel);
-			shadowRoot.appendChild(button);
+			// let shadowRoot = this.attachShadow({ mode: 'closed' });
+			this.appendChild(style);
+			this.appendChild(panel);
+			// this.appendChild(button);
+
+			createLine(panel, 'Void main() => println("Hello world!")');
 		}
 
 	}
 
 	customElements.define('md-editor', MdEditor);
+
+	function createLine(panel, text) {
+		let line = document.createElement('line');
+		let span = document.createElement('span');
+		let space = document.createTextNode(text);
+		span.appendChild(space);
+		line.appendChild(span);
+		panel.appendChild(line);
+		setSelection(span.firstChild, 0);
+	}
+
+	function setSelection(anchor, offset) {
+		let range = document.createRange();
+		let sel = window.getSelection();
+		range.setStart(anchor, offset);
+		range.setEnd(anchor, offset);
+		range.collapse(true);
+		sel.removeAllRanges();
+		sel.addRange(range);
+	}
+
 
 })();
