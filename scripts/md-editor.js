3f293adebe69b8497de19ade0c3e2492fed1bfd0
Feilen <Manuel.Feilen@adesso.de>
2020-06-13 23:22:09 +0200

    changed to closed shadowdom and introduced weakmap

--- scripts/md-editor.js
+++ scripts/md-editor.js
@@ -1,5 +1,7 @@
 (function () {
 
+	let hidden = new WeakMap();
+
 	const ENTER = 'Enter';
 	const TAB_KEY = 'Tab';
 	const BACKSPACE = 'Backspace';
@@ -8,22 +10,16 @@
 
 		constructor() {
 			super();
-		}
 
-		connectedCallback() {
 			let panel = document.createElement('div');
 			panel.classList.add('md-editor');
 			panel.contentEditable = true;
 
-			let button = document.createElement('button');
-			button.innerHTML = "Parse";
-
-			this.addEventListener('keydown', (event) => {
+			panel.addEventListener('keydown', (event) => {
 				switch (event.key) {
 					case ENTER:
 						event.preventDefault();
-						console.log(window.getSelection());
-						createLine(panel, '\xA0');
+						hidden[this].createLine('\xA0');
 						break;
 					case TAB_KEY:
 						event.preventDefault();
@@ -33,61 +29,42 @@
 				}
 			});
 
-			button.addEventListener('click', (event) => {
-				let start = new Date();
-				let scanner = new MdScanner();
-				let tokens = scanner.parse(panel.innerText);
-				panel.innerHTML = mark(tokens);
-				let end = new Date();
-				console.log(`${end.getTime() - start.getTime()} ms`);
-				console.log(tokens);
-			});
-
 			let style = document.createElement('link');
 			style.rel = 'stylesheet';
 			style.href = "styles/md-editor.css";
 
-			// let shadowRoot = this.attachShadow({ mode: 'closed' });
-			this.appendChild(style);
-			this.appendChild(panel);
-			// this.appendChild(button);
+			let shadowRoot = this.attachShadow({ mode: 'closed' });
+			shadowRoot.appendChild(style);
+			shadowRoot.appendChild(panel);
 
-			createLine(panel, 'Void main() => println("Hello world!")');
+			let isFirefox = navigator.userAgent.indexOf("Firefox") != -1;
+			let root = isFirefox ? window : shadowRoot
+
+			hidden[this] = {
+				getSelection: () => getSelection(root),
+				setSelection: (anchor, offset) => setSelection(root, anchor, offset),
+				createLine: (text) => createLine(root, panel, text)
+			};
+		}
+
+		connectedCallback() {
+			hidden[this].createLine('Void main() => println("Hello world!")');
 		}
 
 	}
 
 	customElements.define('md-editor', MdEditor);
 
-	function createLine(panel, text) {
-		let line = document.createElement('line');
-		let span = document.createElement('span');
-		let node = document.createTextNode(text);
-
-		span.appendChild(node);
-		line.appendChild(span);
-
-		let selected = getSelectedLine();
-
-		if (selected != null && selected.nextSibling != null) {
-			panel.insertBefore(line, selected.nextSibling);
-		} else {
-			panel.appendChild(line);
-		}
-
-		setSelection(span.firstChild, 0);
-	}
-
-	function getSelectedLine() {
-		let sel = window.getSelection();
+	function getSelectedLine(root) {
+		let sel = root.getSelection();
 		let node = sel.anchorNode;
 		while (node != null && node.tagName != 'LINE') { node = node.parentNode; }
 		return node;
 	}
 
-	function setSelection(anchor, offset) {
+	function setSelection(root, anchor, offset) {
 		let range = document.createRange();
-		let sel = window.getSelection();
+		let sel = root.getSelection();
 		range.setStart(anchor, offset);
 		range.setEnd(anchor, offset);
 		range.collapse(true);
@@ -95,5 +72,23 @@
 		sel.addRange(range);
 	}
 
+	function createLine(root, panel, text) {
+		let line = document.createElement('LINE');
+		let span = document.createElement('SPAN');
+		let node = document.createTextNode(text);
+
+		span.appendChild(node);
+		line.appendChild(span);
+
+		let selected = getSelectedLine(root);
+
+		if (selected != null && selected.nextSibling != null) {
+			panel.insertBefore(line, selected.nextSibling);
+		} else {
+			panel.appendChild(line);
+		}
+
+		setSelection(root, span.firstChild, 0);
+	}
 
 })();
